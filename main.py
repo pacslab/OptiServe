d4acbe14f37d3fb166e703c9a6415906c001518b
2arian3 <aryan.boukani2@gmail.com>
2024-07-11 20:24:17 -0400

    complete: log fetching.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from src.profiler.profiler import create_functions, profile_function
+from src.profiler.profiler import create_functions, profile_function, get_function_profiling_logs
 
-funcs = create_functions(application_dir='./experiments/applications/A1')
+# funcs = create_functions(application_dir='./experiments/applications/A1')
 
-profile_function(function=funcs[0], num_of_iterations=1)
+# profile_function(function=funcs[0], num_of_iterations=1)
+
+
+get_function_profiling_logs(log_group_name='/aws/lambda/A1_F1')
