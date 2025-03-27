153df24a6ed6ed3bed5bd15a979fbe93a79cd6ae
2arian3 <aryan.boukani2@gmail.com>
2025-03-27 01:11:30 -0400

    delete: main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.profiler.profiler import create_functions, profile_function, get_function_profiling_logs
-
-# funcs = create_functions(application_dir='./experiments/applications/A1')
-
-# profile_function(function=funcs[0], num_of_iterations=1)
-
-
-print(get_function_profiling_logs(log_group_name='/aws/lambda/A1_F1'))
