30ae01b760774a1d0628c6e20af33e91ff201dcc
MoHoss007 <mmhossirca@gmail.com>
2025-04-07 18:18:03 -0400

    Added constraints

---
+++ src/constraints/base_constraint.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from src.utils.function import Function
+from typing import List
+
+
+class BaseConstraint(ABC):
+    """
+    Abstract base class for defining constraints in an optimization problem.
+    """
+
+    @abstractmethod
+    def is_satisfied(self, solution: List[Function]) -> bool:
+        pass
