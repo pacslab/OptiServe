3f8554a31e9abc4a6f20aa99ffea2cb3a3de540e
MoHoss007 <mmhossirca@gmail.com>
2025-04-08 15:53:27 -0400

    Added accuracy to function performance modeling

--- src/optimizer/optimizer.py
+++ src/optimizer/optimizer.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from typing import Dict
 from src.optimizer.objective import Objective
 from src.utils.sampler import Sampler
 from src.utils.logger import logger
@@ -12,88 +12,92 @@ from src.exceptions import (
 class Optimizer:
     def __init__(
         self,
-        objective: Objective,
+        objectives: Dict[str, Objective],
         sampler: Sampler,
         max_total_sample_count: int = 30,
     ):
-        self.objective = objective
+        self.objectives = objectives
         self.sampler = sampler
         self._max_total_sample_count = max_total_sample_count
-        
-    
-    @property
-    def _is_terminated(self):
-        sample_count = len(self.sampler.exploration)
-        print('Sample Count: ', sample_count)
-        print('Max Sample Count: ', self._max_total_sample_count)
-        print('Memories: ', self.sampler.exploration.memories)
-        print('Duration: ', self.sampler.exploration.durations)
-        termination_value = self.objective.termination_value
-        print('Value and Threshold: ', termination_value, self.objective.termination_threshold)
-        return (sample_count > self._max_total_sample_count or termination_value > self.objective.termination_threshold)
-        
-    
-    def _initialize(self):
+
+    def _is_terminated(self, model_name: str):
+        sample_count = len(self.sampler.explorations[model_name])
+        print("Sample Count: ", sample_count)
+        print("Max Sample Count: ", self._max_total_sample_count)
+        print("Memories: ", self.sampler.explorations[model_name].memories)
+        print("Duration: ", self.sampler.explorations[model_name].durations)
+        termination_value = self.objectives[model_name].termination_value
+        print(
+            "Value and Threshold: ",
+            termination_value,
+            self.objectives[model_name].termination_threshold,
+        )
+        return (
+            sample_count > self._max_total_sample_count
+            or termination_value > self.objectives[model_name].termination_threshold
+        )
+
+    def _initialize(self, model_name: str):
         self.sampler.exploration_init()
-        
-        exploration = self.sampler.exploration
-        
-        print(f'Explored memories: {exploration.memories}')
-        
+
+        exploration = self.sampler.explorations[model_name]
+
+        print(f"Explored memories: {exploration.memories}")
+
         for memory in set(exploration.memories):
-            self.objective.update_knowledge(memory)
-        
+            self.objectives[model_name].update_knowledge(memory)
+
         try:
-            self.objective.param_function.fit(exploration)
+            self.objectives[model_name].param_function.fit(exploration)
         except RuntimeError as e:
             logger.error(e.args[0])
             raise RuntimeError("Could not fit the parametric function.")
-        
-        
-    def _update(self, memory_mb: int):
+
+    def _update(self, memory_mb: int, model_name: str):
         try:
             self.sampler.update_exploration(memory_mb)
         except NotEnoughMemory as e:
-            logger.error(f'Trying with new memories. {self.sampler.explorer.invoker._function_name}: {memory_mb}MB')
+            logger.error(
+                f"Trying with new memories. {self.sampler.explorer.invoker._function_name}: {memory_mb}MB"
+            )
             self.memory_space = np.array(
                 [
-                    mem for mem in self.sampler.memory_space if
-                    mem >= self.sampler.memory_space[0] + 128
+                    mem
+                    for mem in self.sampler.memory_spaces[model_name]
+                    if mem >= self.sampler.memory_spaces[model_name][0] + 128
                 ],
-                dtype=int
+                dtype=int,
             )
             return
-        
-        self.objective.update_knowledge(memory_mb)
+
+        self.objectives[model_name].update_knowledge(memory_mb)
         try:
-            self.objective.param_function.fit(self.sampler.exploration)
+            self.objectives[model_name].param_function.fit(
+                self.sampler.explorations[model_name]
+            )
         except RuntimeError as e:
             logger.error(e.args[0])
             raise RuntimeError("Could not fit the parametric function.")
-        
-        
-    def _select_next_memory_to_explore(self):
-        exploration_memories = set(self.sampler.exploration.memories)
-        memory_space = set(self.sampler.memory_space)
-        
+
+    def _select_next_memory_to_explore(self, model_name: str):
+        exploration_memories = set(self.sampler.explorations[model_name].memories)
+        memory_space = set(self.sampler.memory_spaces[model_name])
+
         remainder_memories = np.array(
-            list(memory_space - exploration_memories),
-            dtype=int
+            list(memory_space - exploration_memories), dtype=int
         )
-        
+
         if len(remainder_memories) == 0:
             raise NoMemoryLeft()
-        
-        
-        values = self.objective.get_values(remainder_memories)
-        
+
+        values = self.objectives[model_name].get_values(remainder_memories)
+
         return remainder_memories[np.argmin(values)]
-    
-    
+
     def start(self):
-        self._initialize()
-        
-        while not self._is_terminated:
-            memory = self._select_next_memory_to_explore()
-            self._update(memory)
-            
+        for model_name in self.objectives.keys():
+            self._initialize(model_name=model_name)
+
+            while not self._is_terminated(model_name=model_name):
+                memory = self._select_next_memory_to_explore(model_name=model_name)
+                self._update(memory_mb=memory, model_name=model_name)
--- src/optimizer/parametric_function.py
+++ src/optimizer/parametric_function.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 
 import numpy as np
+from typing import Any, Optional
 from scipy.optimize import curve_fit
-
 from src.utils.exploration import Exploration
 from src.utils.logger import logger
 from src.exceptions import (
@@ -12,54 +12,52 @@ from src.exceptions import (
 
 @dataclass
 class ParamFunction:
-    
-    function: callable = lambda x, a0, a1, a2: (a0 + a1 * np.exp(-x / a2)) if a2 != 0 else a0
+
+    function: callable = lambda x, a0, a1, a2: (
+        (a0 + a1 * np.exp(-x / a2)) if a2 != 0 else a0
+    )
     bounds: tuple = ([-np.inf, -np.inf, -np.inf], [np.inf, np.inf, np.inf])
-    params: any = None
-    
-    
+    params: Any = None
+
     def __call__(self, x: np.ndarray):
         return self.function(x, *self.params)
-    
-    
+
     def fit(self, exploration: Exploration):
         if self.params is None:
             self.params = [exploration.durations[0] // 10] * 3
-            
-            
+
         self.params = curve_fit(
-            f = self.function,
-            xdata = exploration.memories,
-            ydata = exploration.durations,
-            maxfev = int(1e8),
-            p0 = self.params,
-            bounds = self.bounds
+            f=self.function,
+            xdata=exploration.memories,
+            ydata=exploration.durations,
+            maxfev=int(1e8),
+            p0=self.params,
+            bounds=self.bounds,
         )[0]
-        
-        
-    def minimize(self,
-                 memory_space: np.ndarray,
-                 latency_constraint_threshold_ms: float = None):
+
+    def minimize(
+        self,
+        memory_space: np.ndarray,
+        latency_constraint_threshold_ms: Optional[float] = None,
+    ):
 
         exec_time = self(memory_space)
         costs = exec_time * memory_space
-        
+
         if latency_constraint_threshold_ms:
             try:
                 feasible_memories = exec_time < latency_constraint_threshold_ms
-                
+
                 if not np.any(feasible_memories):
-                    raise UnfeasibleConstraint(f"No feasible memory configuration found for latncy requirement {latency_constraint_threshold_ms} ms.")
+                    raise UnfeasibleConstraint(
+                        f"No feasible memory configuration found for latncy requirement {latency_constraint_threshold_ms} ms."
+                    )
 
             except UnfeasibleConstraint as e:
                 logger.warning(e)
-                
+
             else:
                 memory_space = memory_space[feasible_memories]
                 costs = costs[feasible_memories]
-            
+
         return memory_space[np.argmin(costs)]
-    
-    
-    
-    
